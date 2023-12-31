26b965b2c5a32e4c2b855cc2fffc5d9a869ba318
thesureshvadde <vaddesureshvarma@gmail.com>
2023-12-31 14:29:41 +0530

    Hello World

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+## variables naming convention rules.
+# 1. variable names can have only a-z,A-Z,_
+# 2. variable name cannot start with number(0-9)
+# 3. variables by default will not have any datatypes.
+# 4. everything is a string.
+
+
+
 PERSON1=sachin  #variable declaration.
 PERSON2=rohit   #variable=value ==> no scapes.
 
