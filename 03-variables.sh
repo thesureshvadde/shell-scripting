af7bd07fe00bc3ee20aa6b2b9563b449ff6b2a8a
thesureshvadde <vaddesureshvarma@gmail.com>
2023-12-31 16:25:27 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username"
+read USERNAME
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password"
+read -s PASSWORD
+echo "Password entered is: $PASSWORD"
